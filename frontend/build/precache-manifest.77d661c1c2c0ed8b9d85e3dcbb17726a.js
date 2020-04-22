8a05d85e9857b665c1748041897d84edec0a9a9f
rohid-hub <rohidwork303@gmail,com>
2020-04-22 14:46:13 +0600

    Django-Reat-Boilerplate

---
+++ frontend/build/precache-manifest.77d661c1c2c0ed8b9d85e3dcbb17726a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b174d2dddb87057405f44411cd0a381",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31307d55951456f506f1",
+    "url": "/static/css/main.e35b010e.chunk.css"
+  },
+  {
+    "revision": "a2c9df5d9fe8e729d583",
+    "url": "/static/js/2.935ea173.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.935ea173.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31307d55951456f506f1",
+    "url": "/static/js/main.a7ab8f5a.chunk.js"
+  },
+  {
+    "revision": "700d31d1c6f37b7bdc51",
+    "url": "/static/js/runtime-main.84578d5e.js"
+  }
+]);
---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Django React App</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
